549d70688e31b8468bce866a8ac67c4cf17681ac
Anurag Ojha <150668348+Anu8084@users.noreply.github.com>
2025-04-01 11:30:54 +0530

    Update EmailFilterResults.jsx

--- Frontend/src/components/EmailFilterResults.jsx
+++ Frontend/src/components/EmailFilterResults.jsx
@@ -11,7 +11,7 @@ const EmailFilterResults = () => {
 
   const handleSendEmails = async () => {
     try {
-        const response = await fetch('http://localhost:5000/api/faculty/email-filter-results', {
+        const response = await fetch('https://alma-vu.onrender.com/api/faculty/email-filter-results', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ selectedAlumni: results.map(alum => alum._id), emailContent })
